a5a5d0c76142cc46b52eeab399342969a1bfe165
Nitsu47 <135637506+Nitsu47@users.noreply.github.com>
2024-01-28 22:06:00 -0300

    task 8 - str to uppercase

---
+++ 8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        charupper = ord(i)
+        if (96 <= ord(i) <= 123):
+            charupper -= 32
+        print("{:c}".format(charupper), end='')
+    print()
+  
