c17c6b170b8b24ab6f39a1292b0bcf57f2783a94
Nitsu47 <135637506+Nitsu47@users.noreply.github.com>
2024-01-28 22:06:45 -0300

    Delete 8-uppercase.py

--- 8-uppercase.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def uppercase(str):
-    for i in str:
-        charupper = ord(i)
-        if (96 <= ord(i) <= 123):
-            charupper -= 32
-        print("{:c}".format(charupper), end='')
-    print()
-  
