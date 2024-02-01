1208d03dd09586aaae1fa90a43fad3eee69fef25
Nitsu47 <agustraversa2004@gmail.com>
2024-01-31 18:37:56 -0800

    task 2 - prints num of argv and argv

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    num_argv = len(argv)
+    if num_argv == 1:
+        print("{} arguments.".format(num_argv - 1))
+    elif (num_argv == 2):
+        print("{} argument:".format(num_argv - 1))
+    else:
+        print("{} arguments:".format(num_argv - 1))
+    for argindex, arg in enumerate(argv[1:]):
+        print("{}: {}".format(argindex, arg))
