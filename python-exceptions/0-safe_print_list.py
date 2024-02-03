3328afe5350a5bfbab5bcc0bdfa9ccf236f678ad
Nitsu47 <agustraversa2004gmail.com>
2024-02-03 01:52:11 +0000

    for inside try and unnecesary variable delete

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    num_el = 0
-    for i in range(0, x):
-        try:
-            print("{:d}".format(my_list[i]), end='')
-            num_el += 1
-        except Exception:
-            break
-    print()
-    return(num_el)
+    try:
+        for i in range(0, x):
+            print("{}".format(my_list[i]), end='')
+        print("", end="\n")
+        return (x)
+    except:
+        print("", end="\n")
+        return (i)
