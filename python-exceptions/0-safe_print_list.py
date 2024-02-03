631018ed6b5a9b00104acde3e57b5cff712841b8
Nitsu47 <agustraversa2004gmail.com>
2024-02-03 01:30:32 +0000

    Task 0 -  prints x elements of a list using try and except

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    num_el = 0
+    for i in range(0, x):
+        try:
+            print(my_list[i], end='')
+            num_el += 1
+        except Exception:
+            break
+    print()
+    return(num_el)
