93b6b880c04dfc9c0eaef95b06e7ac4871a7520f
Nitsu47 <agustraversa2004@gmail.com>
2024-02-04 22:38:49 +0000

    task 1 - initializates size of square

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Create the class 'Square'"""
+
+
+class Square:
+    """define the Square class"""
+    def __init__(self, size):
+        """initialisates the size of Square"""
+        self.__size = size
