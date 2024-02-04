af32117c6a85b6c0c6559e7fa78b9c5009094405
Nitsu47 <agustraversa2004@gmail.com>
2024-02-04 20:22:03 +0000

    division of two lists and exceptions print error and 0

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -3,14 +3,17 @@ def list_division(my_list_1, my_list_2, list_length):
     i, newlist = 0, []
     while i < list_length:
         try:
-            div = [my_list_1[i] / my_list_2[i]]
+            div = my_list_1[i] / my_list_2[i]
             newlist.append(div)
-        except ValueError:
+        except TypeError:
             print("wrong type")
+            newlist.append(0)
         except IndexError:
             print("out of range")
+            newlist.append(0)
         except ZeroDivisionError:
             print("division by 0")
+            newlist.append(0)
         finally:
             i += 1
     return newlist
