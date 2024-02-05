f799a714e8155647089d9adbe1545b287bfce89e
Nitsu47 <agustraversa2004@gmail.com>
2024-02-05 03:35:10 +0000

    task re-ordered

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""Create the class 'Square'"""
+"""Creates the class 'Square'"""
 
 
 class Square:
-    """define the Square class"""
+    """Defines the Square class"""
+
     def __init__(self, size=0, position=(0, 0)):
-        """initialisates the size of Square"""
+        """Initializes the square instance"""
         self.__size = size
         self.__position = position
 
     def area(self):
-        """defines the area of Square"""
+        """Returns the area of the square"""
         return self.__size ** 2
 
     def my_print(self):
@@ -21,32 +22,32 @@ class Square:
             position = self.position
             if position[1] > 0:
                 for i in range(position[1]):
-                    print("")
+                    print()
             for i in range(self.size):
                 print(" " * position[0] + "#" * self.size)
 
     @property
     def size(self):
-        """returns the size of square"""
+        """Returns the size of the square"""
         return self.__size
-
-    @size.setter
-    def size(self, value):
-        """sets size of square"""
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        if value < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = value
 
     @property
     def position(self):
-        """returns the position of square"""
+        """Retrieves the position of the square"""
         return self.__position
+
+    @size.setter
+    def size(self, value):
+        """Sets the size of the square"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
     @position.setter
     def position(self, value):
-        """sets the position of square"""
+        """sets the position of the square"""
         if not isinstance(value, tuple) or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
         if not isinstance(value[0], int) or not isinstance(value[1], int):
