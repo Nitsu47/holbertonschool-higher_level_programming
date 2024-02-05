42881eafebd4699b451937cc794a9f4b7b09d297
Nitsu47 <agustraversa2004@gmail.com>
2024-02-05 02:16:31 +0000

    task 5 - implementation of area printed with '#'

---
+++ python-classes/5-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Create the class 'Square'"""
+
+
+class Square:
+    """define the Square class"""
+    def __init__(self, size=0):
+        """initialisates the size of Square"""
+        self.__size = size
+
+    def area(self):
+        """defines the area of Square"""
+        return self.__size ** 2
+
+    def my_print(self):
+        """prints the square with '#'"""
+        if self.size == 0:
+            print()
+        else:
+            for i in range(self.size):
+                print("#" * self.size)
+
+    @property
+    def size(self):
+        """returns the size of square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """sets size of square"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
