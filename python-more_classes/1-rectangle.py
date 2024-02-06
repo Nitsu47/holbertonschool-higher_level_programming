ef4fb6776c5db5d8b2ec346638ea7a2a58a27646
Nitsu47 <agustraversa2004@gmail.com>
2024-02-06 08:58:01 -0800

    order inverted

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -6,8 +6,8 @@ class Rectangle:
     """defines the Rectangle class"""
     def __init__(self, width=0, height=0):
         """initializates the rectangle dimensions"""
-        self.__width = width
         self.__height = height
+        self.__width = width
 
     @property
     def width(self):
