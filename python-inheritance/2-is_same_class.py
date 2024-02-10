3b1a4465272e1595b3ced5609784fb7ee493786f
Nitsu47 <agustraversa2004@gmail.com>
2024-02-10 02:40:34 +0000

    under 60 col

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-"""Function to check if an object is an instance of an specified class"""
+"""Function to check if an object is
+an instance of an specified class
+"""
 
 
 def is_same_class(obj, a_class):
-    """Return True if obj is an instance of a_classm, otherwise False"""
+    """Return True if obj is an instance of a_classm
+	otherwise False"""
 
     if type(obj) == a_class:
         return True
