4bbabf9a421fe1a922e47ecef628c572d58b9fb3
Nitsu47 <agustraversa2004@gmail.com>
2024-02-10 04:08:49 +0000

    style correction

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """Checks if an object is an insit ance of a class or
-inhereted from a class
+   inhereted from a class
 """
 
 
 def is_kind_of_class(obj, a_class):
     """Returns True if obj is an instance of a_class or
-    is an instance of a class that inherited from a_class
-    otherwise return False
+       is an instance of a class that inherited from a_class
+       otherwise return False
     """
 
     if type(obj) == a_class or isinstance(obj, a_class):
-        return True
+        return (True)
     else:
-        return False
+        return (False)
