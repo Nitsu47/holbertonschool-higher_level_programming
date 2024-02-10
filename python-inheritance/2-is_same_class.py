16d0273b4151b95838c2f914e573b1e05fe4114f
Nitsu47 <agustraversa2004@gmail.com>
2024-02-10 01:12:48 +0000

    spaces for tabs

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -3,9 +3,9 @@
 
 
 def is_same_class(obj, a_class):
-    """Returns True if the object is exactly an instance, otherwise False"""
+	"""Returns True if the object is exactly an instance, otherwise False"""
 
-    if type(obj) == a_class:
-        return True
-    else:
-        return False
+	if type(obj) == a_class:
+		return True
+	else:
+		return False
