6cb6e4e25b0828269a0a339e397025d18a89f91f
Nitsu47 <agustraversa2004@gmail.com>
2024-02-12 01:52:19 +0000

    task 7 - integer validator and tests for that method

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Creates a new empty 'BaseGeometry' class"""
+
+
+class BaseGeometry:
+    """empty class"""
+    pass
+
+    def area(self):
+        """Raises an Exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates the value, raise exceptions if value is not int"""
+        if type(value) is not int:
+            raise TypeError("<name> must be an integer")
+        if value <= 0:
+            raise ValueError("<name> must be greater than 0")
+        else:
+            return True
