711499a413212d9e387c2d1c378ff5871da86f39
Nitsu47 <agustraversa2004@gmail.com>
2024-02-13 08:47:46 -0800

    documentation fix

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""Creates the Base model to start future clases"""
+"""Creates the Base model to start future classes"""
 
 
 class Base:
+    """Base class to inheritance in future classes"""
     __nb_objects = 0
 
     def __init__(self, id=None):
