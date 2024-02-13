4617ba9ceaaddf511acedd5df893f2568a3f522b
Nitsu47 <agustraversa2004@gmail.com>
2024-02-13 05:50:50 -0800

    task 1 - Base class

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Creates the Base model to start future clases"""
+
+
+class Base:
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """Initializates the object id"""
+        if id is not None:
+            self.id = id  
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects 
