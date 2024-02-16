7b3438099637ebc4805b99c8759b2e7333edad1f
Nitsu47 <agustraversa2004@gmail.com>
2024-02-16 07:48:40 -0800

    serialization of a dict

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Creates the Base model to start future classes"""
+import json
 
 
 class Base:
@@ -13,3 +14,10 @@ class Base:
             self.id = Base.__nb_objects
         else:
             self.id = id
+
+    def to_json_string(list_dictionaries):
+        """returns the JSON string representation of list_dictionaries"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return ("[]")
+        else:
+            return json.dumps(list_dictionaries)
