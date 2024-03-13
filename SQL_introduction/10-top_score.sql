f6fd21128c117e4a42517716564c672898f1b8ae
Nitsu47 <agustraversa2004@gmail.com>
2024-03-12 19:35:31 -0700

    variables to lower

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
--- lists all records of the table second_table.
-SELECT SCORE, NAME FROM second_table
-ORDER BY SCORE DESC;
+-- Lists all records of the table second_table.
+SELECT score, name FROM second_table
+ORDER BY score DESC;
