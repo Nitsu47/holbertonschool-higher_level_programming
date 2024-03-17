965b26717e0afe2f8431138838c958700a4bfcfa
Nitsu47 <agustraversa2004@gmail.com>
2024-03-17 20:12:42 +0000

    correctio  in INNER JOIN, group and order

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,6 @@
 -- Lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
-SELECT tv_show.name AS genre
+SELECT tv_genre.name AS genre
 COUNT (*) AS number_of_shows
-FROM hbtn_0d_tvshows
-GROUP BY genre;
+INNER JOIN genre ON tv_genre.genre_id = genre.id
+GROUP BY genre.name
+ORDER BY number_of_shows DESC;
