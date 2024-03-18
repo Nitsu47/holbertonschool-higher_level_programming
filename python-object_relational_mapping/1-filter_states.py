2186a6fed24c03c99614adb676a67eb4ee786c0d
Nitsu47 <agustraversa2004@gmail.com>
2024-03-18 02:58:31 +0000

    task 1 - All states with a name starting with N

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa """
+if __name__ == '__main__':
+    import sys
+    import MySQLdb
+
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    mysql_db = sys.argv[3]
+
+    data_b = MySQLdb.connect(
+        host="localhost",
+        user=mysql_username,
+        passwd=mysql_password,
+        db=mysql_db,
+        port=3306
+    )
+
+    cursor = data_b.cursor()
+    cursor.execute("SELECT * FROM states WHERE LIKE BINARY %N ORDER BY id ASC")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    data_b.close()
