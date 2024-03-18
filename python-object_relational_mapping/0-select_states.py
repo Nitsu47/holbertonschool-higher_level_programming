f5a7c270379fd182c720d1e91a7bc8c3d4ad454b
Nitsu47 <agustraversa2004@gmail.com>
2024-03-18 02:42:38 +0000

    task 0 - select states from a database

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Lists all states from the database hbtn_0e_0_usa """
+if __name__ == '__main__':
+    import sys
+    import MySQLdb
+
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    mysql_db = sys.argv[3]
+
+    data_b = MySQLdb.connect(
+        host="localhost",
+        user=mysql_username,
+        passwd=mysql_password,
+        db=mysql_db,
+        port=3306
+    )
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
