85c2a4caa2970c4ef853121cb49c11c1adb537e9
Nitsu47 <agustraversa2004gmail.com>
2024-03-30 05:10:07 +0000

    type error fix

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     max = my_list[0]
-    for is_max? in my_list:
-        if is_max? > max:
-            max = is_max?
+    for is_max in my_list:
+        if is_max > max:
+            max = is_max
     return max
