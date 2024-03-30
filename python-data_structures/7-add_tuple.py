da0aa649c0adfaf512824dde60eaf16c0c352d02
Nitsu47 <agustraversa2004gmail.com>
2024-03-30 04:55:36 +0000

    pycodestyle fix

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -2,6 +2,8 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     list_a = list(tuple_a)
     list_b = list(tuple_b)
-    a = (0 if len(list_a) < 1 else list_a[0]) + (0 if len(list_b) < 1 else list_b[0])
-    b = (0 if len(list_a) < 2 else list_a[1]) + (0 if len(list_b) < 2 else list_b[1])
+    a = (0 if len(list_a) < 1 else
+         list_a[0]) + (0 if len(list_b) < 1 else list_b[0])
+    b = (0 if len(list_a) < 2 else
+         list_a[1]) + (0 if len(list_b) < 2 else list_b[1])
     return (a, b)
