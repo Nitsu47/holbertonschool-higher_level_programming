233c6ababb98661b962f6e5fd380c66b0438b5fd
Nitsu47 <agustraversa2004@gmail.com>
2024-03-31 22:31:40 +0000

    last value

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -4,14 +4,16 @@ def roman_to_int(roman_string):
         return 0
     else:
         roman_dict = {'I': 1, 'V': 5, 'X': 10,
-                    'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+                      'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 
         result = 0
+
+        last_value = roman_dict[roman_string[-1]]
 
         for i in range(len(roman_string)-1):
             if roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]:
                 result -= roman_dict[roman_string[i]]
             else:
                 result += roman_dict[roman_string[i]]
-        result += roman_dict[roman_string[-1]]
+        result += last_value
         return result
