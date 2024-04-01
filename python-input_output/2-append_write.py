6d47dfa4b3a48ab611118138125c5e1396c8f557
Nitsu47 <agustraversa2004@gmail.com>
2024-04-01 02:13:07 +0000

    changed name of func

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -2,7 +2,7 @@
 """Appends to a file"""
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     """Open a given file and append on it"""
     with open(filename, "a", encoding="utf-8") as file:
         nums = file.write(text)
