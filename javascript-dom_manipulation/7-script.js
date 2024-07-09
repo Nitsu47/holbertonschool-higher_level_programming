71dadc7aa98a66424a32c2b0c55e278c30805ad9
Nitsu47 <agustraversa2004@gmail.com>
2024-07-08 17:40:46 -0700

    Task 7 - List of star wars movies

---
+++ javascript-dom_manipulation/7-script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const listMovies = document.getElementById("list_movies");
+
+  fetch('https://swapi-api.hbtn.io/api/films/?format=json')
+    .then(res => res.json())
+    .then(data => {
+      data.results.forEach(movie => {
+        const li = document.createElement('li');
+        li.textContent = movie.title;
+        listMovies.appendChild(li);
+      });
+    })
+});
